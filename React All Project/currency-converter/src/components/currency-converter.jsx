c4591165c7f4eda64c3af9530d0ad65e3af22290
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-10-25 12:46:31 +0530

    final commit

--- React All Project/currency-converter/src/components/currency-converter.jsx
+++ React All Project/currency-converter/src/components/currency-converter.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import  { useState } from 'react'
 import CurrencyDropdown from './currency-dropdown';
+import { IoMdSwap } from "react-icons/io";
 
 
  const CurrencyConverter = () => {
@@ -8,6 +9,11 @@ import CurrencyDropdown from './currency-dropdown';
 
     const [currencies,setCurrencies] = useState([]);
     const [amount, setamount] = useState(1);
+    const [fromCurrency,setFromCurrency] = useState("USD");
+    const [toCurrency,setTOCurrency] = useState("INR");
+    const [convertedAmount, setConvertedAmount] = useState(null);
+    const [convertionMoment, setConvertionMoment] = useState(false);
+    
 
     // function to fetch curriences from API
     const fetchCurrencies = async()=>{
@@ -29,12 +35,35 @@ import CurrencyDropdown from './currency-dropdown';
     },[]);
 
     // check the curriencies listed or not
-    console.log(currencies);
+    //console.log(currencies);
 
     // convert the currencie
     const convertCurrency = async ()=>{
-
+        // if amount is not present then no convertion 
+        if(!amount) return;
+        // else first change the convertion moment to true
+        // and start the convertion process
+        setConvertionMoment(true);
+        // Actual Convertion is start
+        try {
+            const result = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`);
+            const data = await result.json();
+            // convert the data
+            setConvertedAmount(data.rates[toCurrency] + " " + toCurrency)
+            
+        } catch (error) {
+            console.error("Error fetching", error);
+        }
+        finally{
+            setConvertionMoment(false);
+        }
     }
+
+    // final to swap  currencies 
+    const swapCurrencies = ()=>{
+        setFromCurrency(toCurrency);
+        setTOCurrency(fromCurrency);
+    };
 
 
   return (
@@ -42,9 +71,22 @@ import CurrencyDropdown from './currency-dropdown';
                     {/*title of application*/} 
         <h2 className='mb-5 text-2xl font-semibold text-gray-700'>Currency Converter</h2>
             {/*List of country from API*/}
-        <div>
-            <CurrencyDropdown title="From:" currencies={currencies}/>
-            <CurrencyDropdown title="To:" currencies={currencies}/>
+        <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 items-end'>
+            <CurrencyDropdown title="From:" currencies={currencies} currency={fromCurrency} setCurrency={setFromCurrency}/>
+
+            {/* SWAP Currency Button */}
+
+            <div className='flex justify-center -mb-5 sm:mb-0'>
+                <button
+                    onClick={swapCurrencies}
+                    className='p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300'
+                >
+                    {/* swap icon */}
+                    <IoMdSwap className='text-xl text-gray-700'/>
+                </button>
+            </div>
+
+            <CurrencyDropdown title="To:" currencies={currencies} currency={toCurrency} setCurrency={setTOCurrency}/>
         </div>
             {/*Amount input Field*/}
         <div>
@@ -59,18 +101,24 @@ import CurrencyDropdown from './currency-dropdown';
         {/*Convert Button*/}
         <div className='flex justify-end mt-6'>
             <button 
-            className='px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 focus:ring-offset-2 font-semibold'
+            className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 focus:ring-offset-2 font-semibold
+                ${convertionMoment ? "animate-ping" : ""}
+            `}
             onClick={convertCurrency}
             >
              Convert</button>
         </div>
 
         {/*Display converted amount*/}
-        <div className='mt-5 text-lg font-medium text-right text-gray-600'>
-            Converted Amount : 69 USD
+        {convertedAmount && (
+            <div className='mt-5 text-lg font-medium text-right text-gray-600'>
+            Converted Amount : {convertedAmount}
         </div>
+        )}
+        
     </div>
   )
 }
 
 export default CurrencyConverter
+
--- React All Project/currency-converter/src/components/currency-dropdown.jsx
+++ React All Project/currency-converter/src/components/currency-dropdown.jsx
@@ -14,7 +14,10 @@ import React from 'react'
         {title} </label>
 
         <div className='mt-1 relative'>
-            <select className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 mt-1 focus:ring-2'>
+            <select className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 mt-1 focus:ring-2'
+                value={currency}
+                onChange={(e)=>setCurrency(e.target.value)}
+                >
                 {currencies?.map((currency)=>{
                     return(
                         <option value={currency} key={currency}> {currency}</option>
