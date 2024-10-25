1499cb2215c9fef1b1cc6b2b0f79313d4694cb76
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-10-25 09:42:55 +0530

    9th commit

--- React All Project/currency-converter/src/components/currency-converter.jsx
+++ React All Project/currency-converter/src/components/currency-converter.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import  { useState } from 'react'
+import CurrencyDropdown from './currency-dropdown';
+
 
  const CurrencyConverter = () => {
+             {/*State Variable*/} 
+
+    const [currencies,setCurrencies] = useState([]);
+    const [amount, setamount] = useState(1);
+
+    // function to fetch curriences from API
+    const fetchCurrencies = async()=>{
+        try {
+            // step - 2  fetch and convert the data according to js
+            const result = await fetch("https://api.frankfurter.app/currencies");
+            const data = await result.json();
+            // update the data
+            setCurrencies(Object.keys(data));
+        } catch (error) {
+            console.error("Error Featching", error);
+        }
+    }
+
+
+    // call the fetch currency method to check the API working method work or not
+    useEffect(()=>{
+        fetchCurrencies();
+    },[]);
+
+    // check the curriencies listed or not
+    console.log(currencies);
+
+    // convert the currencie
+    const convertCurrency = async ()=>{
+
+    }
+
+
   return (
-    <div>
-        <h2>Hello World</h2>
+    <div className='max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md '>
+                    {/*title of application*/} 
+        <h2 className='mb-5 text-2xl font-semibold text-gray-700'>Currency Converter</h2>
+            {/*List of country from API*/}
+        <div>
+            <CurrencyDropdown title="From:" currencies={currencies}/>
+            <CurrencyDropdown title="To:" currencies={currencies}/>
+        </div>
+            {/*Amount input Field*/}
+        <div>
+            <label htmlFor="amount"
+            className='block text-sm font-medium text-gray-700'
+            >Amount</label>
+            <input type="number" 
+            className='w-full p-2 border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-indigo-500 mt-1 focus:ring-2'
+            value={amount} onChange={(e)=>setamount(e.target.value)}/>
+        </div>
+
+        {/*Convert Button*/}
+        <div className='flex justify-end mt-6'>
+            <button 
+            className='px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 focus:ring-offset-2 font-semibold'
+            onClick={convertCurrency}
+            >
+             Convert</button>
+        </div>
+
+        {/*Display converted amount*/}
+        <div className='mt-5 text-lg font-medium text-right text-gray-600'>
+            Converted Amount : 69 USD
+        </div>
     </div>
   )
 }
---
+++ React All Project/currency-converter/src/components/currency-dropdown.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+ const CurrencyDropdown = ({
+    currencies,
+    currency,
+    setCurrency,
+    title=""
+ }) => {
+  return (
+    <div>
+        {/* label for country of currencies */}
+        <label htmlFor={title}
+        className='block text-sm font-medium text-gray-700'
+        > 
+        {title} </label>
+
+        <div className='mt-1 relative'>
+            <select className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 mt-1 focus:ring-2'>
+                {currencies?.map((currency)=>{
+                    return(
+                        <option value={currency} key={currency}> {currency}</option>
+                    );
+                })}
+            </select>
+        </div>
+
+    </div>
+  )
+}
+
+export default CurrencyDropdown
